1a081e4cf0a7bfcb5d17e88c2cea81d5bd08eae6
Фаиль <fail.gataullin@amopoint.ru>
2022-10-07 20:50:35 +0500

    struct

--- main.go
+++ main.go
@@ -5,8 +5,26 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name     string
+	age      uint16
+	happines float32
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("My name is %s. Happines: %f", u.name, u.happines)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
+	bob := User{"Bob", 25, 0.15}
+
+	bob.setNewName("Sam")
+
+	fmt.Fprintf(w, bob.getAllInfo())
 }
 
 func about_page(w http.ResponseWriter, r *http.Request) {
