26ce5d02f9183e91c7ec6dee7dc30eb5f09eb43b
Фаиль <fail.gataullin@amopoint.ru>
2022-10-07 20:17:07 +0500

    net/http

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func about_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Page")
+}
+
+func initHadlers() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/about/", about_page)
+	http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-	fmt.Println("Hello www")
+	initHadlers()
 }
